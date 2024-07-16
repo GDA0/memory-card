31ccb1caa2d20fee5984f9b9ba8c90fa47c77b11
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-16 09:24:03 +0000

    Work on footer contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,18 @@
 export default function Footer () {
-
+  return (
+    <footer className='text-center p-3 fixed-bottom bg-white'>
+      <p className='mb-0'>
+        &copy; {new Date().getFullYear()} Memory Card. Created By{' '}
+        <a
+          className='text-primary'
+          href='https://github.com/GDA0'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Gideon D. Adeti
+        </a>
+        . All Rights Reserved.
+      </p>
+    </footer>
+  )
 }
