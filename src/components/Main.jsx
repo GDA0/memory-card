2593032b15e95d1f79838b35a91edee2f5b91387
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-16 09:44:05 +0000

    Work on main contents; add instructions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,10 @@
 export default function Main () {
-
+  return (
+    <main className='my-5 py-3 container'>
+      <p>
+        Get points by clicking on an image, but do <b>not</b> click on any more
+        than once!
+      </p>
+    </main>
+  )
 }
