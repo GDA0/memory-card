806c90bcafb54a5cee3f9f9fdc96332614bf835a
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-16 09:29:34 +0000

    Work on header contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,11 @@
-export default function Header () {
-
+export function Header () {
+  return (
+    <header>
+      <nav className='navbar fixed-top bg-white container-fluid border-bottom border-1'>
+        <div className='container d-flex justify-content-between'>
+          <span className='navbar-brand mb-0 h1'>Memory Card</span>
+        </div>
+      </nav>
+    </header>
+  )
 }
