104807434922ce4d9bbe91528b412cc37cb3fad9
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-16 11:10:15 +0000

    Update Main Component to Display Characters
    Implement character fetching in the App component using useEffect and
    manage state with useState. Characters are passed to the Main component
    as props and displayed in the UI as cards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { Header } from './components/Header'
-import Main from './components/Main'
+import { Main } from './components/Main'
 import { Footer } from './components/Footer'
 
+import { useState, useEffect } from 'react'
+
+import { fetchCharacters } from './fetch-characters'
+
 export default function App () {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    async function getCharacters () {
+      const fetchedCharacters = await fetchCharacters()
+      setCharacters(fetchedCharacters)
+    }
+    getCharacters()
+  }, [])
+
   return (
     <>
       <Header />
-      <Main />
+      <Main characters={characters} />
       <Footer />
     </>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,34 @@
-export default function Main () {
+export function Main ({ characters }) {
+  if (!Array.isArray(characters)) {
+    return (
+      <div className='spinner-border' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </div>
+    )
+  }
+
+  const characterCards = characters.map((character) => (
+    <div key={character.id} className='col'>
+      <div className='card h-100'>
+        <img
+          className='card-img-top'
+          src={character.image}
+          alt={character.name}
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>{character.name}</h5>
+        </div>
+      </div>
+    </div>
+  ))
+
   return (
     <main className='my-5 py-3 container'>
       <p>
         Get points by clicking on an image, but do <b>not</b> click on any more
         than once!
       </p>
+      <div className='row row-cols-4 g-4'>{characterCards}</div>
     </main>
   )
 }
